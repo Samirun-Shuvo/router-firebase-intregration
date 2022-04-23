f2cc1e2120794a9078b69aee3c216cf65fa4dbe8
Samirun-Shuvo <samirunshuvo@gmail.com>
2022-04-24 05:55:30 +0600

    on auth state change updated

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB05DfBsQZvdOr2C7RmuJr_i7nwzgXw3Go",
+    authDomain: "router-firebase-intregra-31e1e.firebaseapp.com",
+    projectId: "router-firebase-intregra-31e1e",
+    storageBucket: "router-firebase-intregra-31e1e.appspot.com",
+    messagingSenderId: "440609267739",
+    appId: "1:440609267739:web:b8be04ea7e2590af3998be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
